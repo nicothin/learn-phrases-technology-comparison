b133f4a64c0898d89708d1202f7390271746e575
Nikolay Gromov <nicothin@gmail.com>
2025-01-26 11:29:06 +0300

    Add stylelint

--- stylelint.config.js
+++ stylelint.config.js
@@ -1,6 +1,11 @@
 /** @type {import('stylelint').Config} */
 export default {
-  extends: 'stylelint-config-standard',
+  extends: [
+    'stylelint-config-standard',
+    'stylelint-config-recommended-vue',
+    'stylelint-order',
+  ],
+  plugins: ['stylelint-order'],
   overrides: [
     {
       files: ['**/*.ts', '**/*.tsx'],
@@ -29,6 +34,7 @@ export default {
     'custom-property-empty-line-before': null,
     'font-family-name-quotes': 'always-unless-keyword',
     'declaration-block-no-redundant-longhand-properties': null,
+    "selector-class-pattern": null
   },
-  ignoreFiles: ['storybook-static/**/*', 'coverage/**/*', 'dist/**/*', 'node_modules/**/*'],
+  ignoreFiles: ['dist/**/*', 'node_modules/**/*'],
 };
