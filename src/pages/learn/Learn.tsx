5fe2a27fd62c5de7e74f7ba98c1e328b19fcd7c9
Nikolay Gromov <nicothin@gmail.com>
2025-02-02 21:39:23 +0300

    Add Learn & Admin markup

--- src/pages/learn/Learn.tsx
+++ src/pages/learn/Learn.tsx
@@ -3,7 +3,15 @@ import React from 'react';
 import './Learn.css';
 
 const Learn: React.FC = () => {
-  return <div>Страница "Learn"</div>;
+  return (
+    <div className="learn">
+
+      <p className="learn__empty">
+        <span>There are no phrases here yet. Go to <a href="/#/admin">the admin section</a> and add a few.</span>
+      </p>
+
+    </div>
+  );
 };
 
 export default Learn;
