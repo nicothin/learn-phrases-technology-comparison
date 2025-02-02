502e510c280b49220bdfee72ffd9d5c7c05fb2b7
Nikolay Gromov <nicothin@gmail.com>
2025-02-02 21:34:34 +0300

    Add Learn & Admin markup

--- src/pages/Learn/Learn.tsx
+++ src/pages/Learn/Learn.tsx
@@ -1,7 +1,15 @@
 import './Learn.css';
 
 const Learn = () => {
-  return <div>Страница "Learn"</div>;
+  return (
+    <div class="learn">
+
+      <p class="learn__empty">
+        <span>There are no phrases here yet. Go to <a href="/#/admin">the admin section</a> and add a few.</span>
+      </p>
+
+    </div>
+  );
 };
 
 export default Learn;
